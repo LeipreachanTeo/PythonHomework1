0b025d27f5a8e58c02d57c893648ed5e0cd694b6
LeipreachanTeo <pokiboom@mail.ru>
2022-10-08 23:59:21 +0700

    fifth task update

--- homework1/second_task.py
+++ homework1/second_task.py
@@ -1,16 +1,7 @@
 # Напишите программу для. проверки истинности утверждения ¬(X ⋁ Y ⋁ Z) = ¬X ⋀ ¬Y ⋀ ¬Z для всех значений предикат.
-def check_predict(x, y, z):
-    left_part = not (x or y or z)
-    right_part = not x and not y and not z
-    result = left_part == right_part
-    return result
-
-
-x, y, z = map(int, input('Ender x,y,z: ').split())
-
-check_predict(x, y, z)
-
-if check_predict(x, y, z) == True:
-    print(f"Statement is True")
-else:
-    print(f"Statement if False")
+for x in [True,False]:
+    for y in [True,False]:
+        for z in [True,False]:
+            left_part = not(x or y or z)
+            right_part = not x and not y and not z
+            print(f' x ={x} y = {y} z = {z} ответ = {left_part == right_part}')
