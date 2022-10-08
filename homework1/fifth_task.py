5392acf8aa99fa4bef9eabe3e511621e3e47f838
LeipreachanTeo <pokiboom@mail.ru>
2022-10-08 17:36:35 +0700

    fifth task

---
+++ homework1/fifth_task.py
@@ -0,0 +1,10 @@
+# Напишите программу, которая принимает на вход координаты двух точек и находит расстояние между ними в 2D пространстве.
+def find_distance(A,B):
+    distance = ((B[0]- A[0])**2 + (B[1]-A[1])**2)**(0.5)
+    return print(f'distance between points A and B = {round(distance,3)}')
+
+
+A = list(map(int,input("Enter the coordinates of point A(x and y) ").split()))
+B = list(map(int,input("Enter the coordinates of point B(x and y)").split()))
+
+find_distance(A,B)
