0b8f3863dc9514c5190c11c9013b5c0c76091f01
LeipreachanTeo <pokiboom@mail.ru>
2022-10-08 15:42:23 +0700

    update

--- homework/first_task.py
+++ homework/first_task.py
@@ -1,2 +1,13 @@
 # Напишите программу, которая принимает на вход цифру, обозначающую день недели, и проверяет, является ли этот день выходным.
-print('hello world')
+def search_weekend(week_number):
+    if 0 < week_number <=5:
+        return "workind day"
+    elif  6 <= week_number <=7:
+        return "weekend"
+    else:
+        return "number doesn't match the day of the week"
+
+
+day = int(input("ender day of the week: "))
+
+print(search_weekend(day))
