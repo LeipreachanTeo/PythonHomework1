dd13f8b108d9dd1f4f9cafd96fb075a958c5ffe3
LeipreachanTeo <pokiboom@mail.ru>
2022-10-12 14:38:28 +0700

    first task homework2

---
+++ Homework2/first_task.py
@@ -0,0 +1,24 @@
+# 1) Напишите программу, которая принимает на вход вещественное число и показывает сумму его цифр.
+def is_float(value):
+    try:
+        float(value)
+        return True
+    except ValueError:
+        return False
+
+
+def sum_digit(n: float) -> int:
+    return sum([int(i) for i in str(n) if i.isdigit()])
+
+
+digit = input("Введите вещественное число: ")
+
+while not is_float(digit):
+    if ',' in digit:
+        print("Введено не вещественное число,вместо \",\" поставьте \".\" и повторите попытку")
+        digit = input("Введите вещественное число: ")
+    else:
+        print("Введено не вещественное число, повторите ввод")
+        digit = input("Введите вещественное число: ")
+
+print(sum_digit(digit))
