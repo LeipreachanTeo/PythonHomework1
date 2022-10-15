ed76c75d61aaf0b3d1bcd1f5ca8d16d639d0132f
LeipreachanTeo <pokiboom@mail.ru>
2022-10-15 17:44:57 +0700

    first task

---
+++ Homework3/first_task.py
@@ -0,0 +1,12 @@
+# Задайте список из нескольких чисел. Напишите программу, которая найдёт сумму элементов списка, стоящих на нечётной позиции.
+def sum_odd_pos_index(list):
+    sum = 0
+    for index, digit in enumerate(list):
+        if index % 2 != 0:
+            sum += digit
+    return sum
+
+
+list_digit = map(int, input("Введите список чисел через проблем: ").split())
+print(sum_odd_pos_index(list_digit))
+
